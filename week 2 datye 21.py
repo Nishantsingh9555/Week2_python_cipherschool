ec98d8b3ce70990f8b2a2cb5543ce9ed3112b47a
Nishantsingh9555 <115680113+Nishantsingh9555@users.noreply.github.com>
2022-12-12 22:11:44 +0530

    Add files via upload

---
+++ week 2 datye 21.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'nishant',
+    'age': 21,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'haryana', 'hobbies' : ['coding','reading','guiter']}
+user_info.update({})
+print(user_info)
